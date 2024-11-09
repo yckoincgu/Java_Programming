dad85505dae637097c977f4ae72c14aec64c9746
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2024-11-09 21:41:40 +0800

    20241109_5
    20241109_5

--- 2024/JavaProject/src/examplesPackage/App.java
+++ 2024/JavaProject/src/examplesPackage/App.java
@@ -3,41 +3,32 @@ package examplesPackage;
 import java.util.Scanner;
 
 
+enum EX {xbasic, xarray, xterminated, xcalculator}
 
 public class App {
 
 	public static void main(String[] args) {
 
-		enum ExampleTYpes {basicTypes, arraytypes, terminated}
 		
-        ExampleTYpes choiceType= ExampleTYpes.basicTypes; 
-        Scanner reader = new Scanner(System.in);
-        int choiceTypeValue=1;
+		EX[] ex=EX.values();
 
-        System.out.println("Execute basic subroutine by inputing 1 \n"
-    			+ "Execute array subroutine by inputing 2 \n"
-    			+ "Terminate by inputing 0 \n"); 
-    	System.out.println();
-    	System.out.println("Please choose a type to execite ...");
-        choiceTypeValue = reader.nextInt();  
-        if ( choiceTypeValue == 0) choiceType= ExampleTYpes.terminated;
-        if ( choiceTypeValue == 1) choiceType= ExampleTYpes.basicTypes;
-        if ( choiceTypeValue == 2) choiceType= ExampleTYpes.arraytypes;
-        
-        switch(choiceType.name()) {
-        case "basicTypes"->{
-        	ExamplesBasic a=new ExamplesBasic();
-        	a.EX03();
-        	a.EX05();
+        for(int i=0; i< EX.values().length; i++) {
+        	System.out.println(i+" "+ex[i].name()); 
         }
-        case "arraytypes"->{
-        	ExamplesArray b=new ExamplesArray();
-        	b.EX45();
-    		b.EX05();    	
-        } 
-        case "terminated"->{
-        	System.out.println("No choice types for your");}
-        }				
+        System.out.println("Choose an integer to run x_application");        
+    
+        Scanner reader = new Scanner(System.in);
+        int choice;    	
+        choice = reader.nextInt();  
+        switch(choice) {
+	        case 3->{
+	        	ExCalculator a = new ExCalculator();
+	        	a.calEntrancec();
+	        }
+        }
+        
+        
+				
 		
         reader.close();
        
