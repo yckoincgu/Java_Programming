85f69b070e876f36a278b057b87622473b5638e4
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2025-08-01 16:47:43 +0800

    20250801_1
    20250801_1

---
+++ test/0_hello.java
@@ -0,0 +1,6 @@
+// Main.java (JDK 24 preview features may require flags)
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Running on JDK " + Runtime.version());
+    }
+}
